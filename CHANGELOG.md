92c2686b6f4da890aa1260c51c746d8a6522fc03
JasonSkillman <jason_skillman@outlook.com>
2020-12-03 14:51:08 -0500

    Add example MonoBehavior script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # State Machine Callback Changelog
 
+## [1.1.0] - 12/03/2020
+- Created example state machine `MonoBehavior` class.
+
 ## [1.0.0] - 11/06/2020
 - State machine callback release.
---
+++ Runtime/Scripts/StateMachine/StateMachineUnityEvents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace StateMachine.Callback {
+	public class StateMachineUnityEvents : MonoBehaviour, IStateMachineCallback {
+
+		[SerializeField]
+		private string filter;
+		[SerializeField]
+		private int layerIndex;
+
+		[SerializeField, Space]
+		private UnityEvent onAnimationStart;
+		[SerializeField]
+		private UnityEvent onAnimationUpdate;
+		[SerializeField]
+		private UnityEvent onAnimationEnd;
+		
+		public void OnAnimationStart(AnimatorStateInfo stateInfo, int layerIndex) {
+			if(filter.Equals(string.Empty) || stateInfo.IsName(filter) && this.layerIndex == layerIndex) {
+				onAnimationStart?.Invoke();
+			}
+		}
+
+		public void OnAnimationUpdate(AnimatorStateInfo stateInfo, int layerIndex) {
+			if(filter.Equals(string.Empty) || stateInfo.IsName(filter) && this.layerIndex == layerIndex) {
+				onAnimationUpdate?.Invoke();
+			}
+		}
+
+		public void OnAnimationEnd(AnimatorStateInfo stateInfo, int layerIndex) {
+			if(filter.Equals(string.Empty) || stateInfo.IsName(filter) && this.layerIndex == layerIndex) {
+				onAnimationEnd?.Invoke();
+			}
+		}
+		
+	}
+}
